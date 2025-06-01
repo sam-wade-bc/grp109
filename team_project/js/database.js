d10786c59cdf15485ce2a0e6829df16a32b4219e
sam-wade-bc <sam.wade@bellevuecollege.edu>
2025-05-31 19:04:38 -0700

    Initial Push

---
+++ team_project/js/database.js
@@ -0,0 +1,36 @@
+// resources.js
+
+var resources = [
+  {
+    title: "Upper Body Strength Workout",
+    description: "A 30-minute dumbbell workout to build upper body strength.",
+    link: "https://www.youtube.com/watch?v=UoC_O3HzsH0",
+    tags: ["Upper Body", "Strength", "15-30 mins"]
+  },
+  {
+    title: "Core and Midsection Flexibility",
+    description: "Stretch routine for flexibility in the abs and lower back.",
+    link: "https://www.youtube.com/watch?v=0UE3CNu_rtY",
+    tags: ["Mid Body", "Flexibility", "15-30 mins"]
+  },
+  {
+    title: "Lower Body Endurance Training",
+    description: "Build stamina with this high-rep leg workout.",
+    link: "https://www.youtube.com/watch?v=2pLT-olgUJs",
+    tags: ["Lower Body", "Stamina", "45 mins - 1 hr"]
+  },
+  {
+    title: "Full Body Power Yoga",
+    description: "A flow to enhance full body strength and flexibility.",
+    link: "https://www.youtube.com/watch?v=v7AYKMP6rOE",
+    tags: ["Upper Body", "Mid Body", "Flexibility", "45 mins - 1 hr"]
+  },
+  {
+    title: "Leg Day Strength Training",
+    description: "Intense lower body strength workout with weights.",
+    link: "https://www.youtube.com/watch?v=2z8JmcrW-As",
+    tags: ["Lower Body", "Strength", "2+ hrs"]
+  }
+];
+
+export default resources;
