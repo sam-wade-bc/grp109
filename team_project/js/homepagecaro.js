b922f4076eeeb0978f621751f7bac2f0d41c4d8f
sam-wade-bc <sam.wade@bellevuecollege.edu>
2025-06-21 14:42:52 -0700

    Update homepagecaro.js

--- team_project/js/homepagecaro.js
+++ team_project/js/homepagecaro.js
@@ -1,80 +1,77 @@
-var myImages =
-["https://sam-wade-bc.github.io/grp109/team_project/images/samW.png",
- "https://sam-wade-bc.github.io/grp109/team_project/images/navdeepS.png"];
+document.addEventListener("DOMContentLoaded", function () {
+  var myImages = [
+    "https://sam-wade-bc.github.io/grp109/team_project/images/samW.png",
+    "https://sam-wade-bc.github.io/grp109/team_project/images/navdeepS.png"
+  ];
 
+  var captions = [
+    "Sam Wade: Team Leader, Home Page, Subscriptions, Custom Workouts",
+    "Navdeep Singh: Design Leader, Layout, Subscriptions"
+  ];
 
-var captions =
-["Sam Wade: Team Leader, Home Page, Subscriptions, Custom Workouts",
- "Navdeep Singh: Design Leader, Layout, Subscriptions"];
+  var altText = [
+    "Picture of Sam Wade",
+    "Picture of Navdeep Singh"
+  ];
 
-var altText = 
-["Picture of Sam Wade",
- "Picture of Navdeep Singh"];
+  var index = 0;
+  var timerDuration = 4000;
+  var countdown = timerDuration / 1000;
+  var timerInterval;
 
- var index=0; 
- var timerDisplay = document.getElementById('timer');
- var timerDuration = 4000;
- var timerInterval;
- var nextButton = document.getElementById("next"); 
- var previousButton = document.getElementById("previous"); 
- var countdown = timerDuration/1000;
- var auto = document.getElementById("auto");
- document.getElementById("auto").addEventListener("change", autoSlide);
+  var timerDisplay = document.getElementById("timer");
+  var nextButton = document.getElementById("next");
+  var previousButton = document.getElementById("previous");
+  var auto = document.getElementById("auto");
 
- function updateImage(){
- document.getElementById("slideshow").src = myImages[index];
- document.getElementById("slideshow").alt= altText[index];
- document.getElementById("caption").textContent = captions[index]; 
-} 
+  document.getElementById("auto").addEventListener("change", autoSlide);
+  previousButton.addEventListener("click", back, false);
+  nextButton.addEventListener("click", next, false);
 
-function next(){
- if (auto.checked)
- startTimer();
- if (myImages.length == index+1)
- index=0;
- else
- index++;
- updateImage();
-} 
- 
-
-function back(){
- if (auto.checked)
- startTimer();
- if (index===0)
- index=myImages.length-1;
- else
- index--;
- updateImage();
-} 
-
-
-previousButton.addEventListener("click",back,false);
-nextButton.addEventListener("click",next,false); 
-
-function autoSlide() {
-  if (document.getElementById("auto").checked) {
-    next(); // Go to next slide when checked
-    startTimer(); // Start the timer
-  } else {
-    stopTimer(); // Stop the timer when checkbox is unchecked
+  function updateImage() {
+    document.getElementById("slideshow").src = myImages[index];
+    document.getElementById("slideshow").alt = altText[index];
+    document.getElementById("caption").textContent = captions[index];
   }
-}
 
-function stopTimer() {
-  clearInterval(timerInterval);
-  timerDisplay.textContent = "Automatic Slides Paused";
-}
+  function next() {
+    if (auto.checked) startTimer();
+    index = (index + 1) % myImages.length;
+    updateImage();
+  }
 
-function startTimer() {
-  clearInterval(timerInterval);
-  var duration = countdown; 
-  timerInterval = setInterval(function () {
-    timerDisplay.textContent = "Next slide in: " + duration + "s";
-    duration--; // Decrement the duration
-    if (duration < 0) {
-      duration = countdown; // Reset the countdown after it reaches 0
-      next(); // Move to the next slide
+  function back() {
+    if (auto.checked) startTimer();
+    index = (index - 1 + myImages.length) % myImages.length;
+    updateImage();
+  }
+
+  function autoSlide() {
+    if (auto.checked) {
+      next();
+      startTimer();
+    } else {
+      stopTimer();
     }
-  }, 1000);
-}
+  }
+
+  function stopTimer() {
+    clearInterval(timerInterval);
+    timerDisplay.textContent = "Automatic Slides Paused";
+  }
+
+  function startTimer() {
+    clearInterval(timerInterval);
+    var duration = countdown;
+    timerInterval = setInterval(function () {
+      timerDisplay.textContent = "Next slide in: " + duration + "s";
+      duration--;
+      if (duration < 0) {
+        duration = countdown;
+        next();
+      }
+    }, 1000);
+  }
+
+  updateImage(); // Initialize on first load
+});
